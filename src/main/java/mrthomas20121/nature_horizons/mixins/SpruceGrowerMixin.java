ea8d24eb16b7d5bd07e17e94564529ef5701ddc9
Thomas B <thomas77300@gmail.com>
2026-04-08 21:51:21 +0200

    added mixin for the spruce grower

---
+++ src/main/java/mrthomas20121/nature_horizons/mixins/SpruceGrowerMixin.java
@@ -0,0 +1,21 @@
+package mrthomas20121.nature_horizons.mixins;
+
+import net.minecraft.data.worldgen.features.TreeFeatures;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.grower.SpruceTreeGrower;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SpruceTreeGrower.class)
+public class SpruceGrowerMixin {
+
+    @Inject(method = "getConfiguredMegaFeature(Lnet/minecraft/util/RandomSource;)Lnet/minecraft/resources/ResourceKey;", at = @At("RETURN"), cancellable = true)
+    private void nature_horizons_getConfiguredMegaFeature(RandomSource p_255928_, CallbackInfoReturnable<ResourceKey<ConfiguredFeature<?, ?>>> cir) {
+        // SpurceTreeGrower can create a mega mine tree so we make it only return a mega spruce
+        cir.setReturnValue(TreeFeatures.MEGA_SPRUCE);
+    }
+}
